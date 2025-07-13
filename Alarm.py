4ef897539395a47b65b09b4bd4897454e6e65bf6
msalarkhan10 <msalarkhan10@gmail.com>
2025-07-13 14:06:34 +0500

    Lesson 1
    IT WAS EZ

---
+++ Alarm.py
@@ -0,0 +1,13 @@
+from tkinter.ttk import *
+from tkinter import *
+
+bg_colour = "#ffffff"
+
+
+window = Tk()
+window.title("Alarm Clock")
+window.configure(bg=bg_colour)
+window.geometry('350x150')
+
+window.mainloop()
+
